fa8d9061b440889f186df47cc21dc5735481044b
Senne Christiaens <senne.christiaens@icloud.com>
2023-05-10 23:01:47 +0200

    feat: add routes to routesview

--- src/views/RoutesView.vue
+++ src/views/RoutesView.vue
@@ -1,7 +1,150 @@
 <script setup>
-import Navigation from '../components/Navigation.vue';
+import Navigation from "../components/Navigation.vue";
 </script>
 
 <template>
-<Navigation v-bind:active="'routes'" style="z-index: 1000;" />
+  <div class="flex">
+    <Navigation v-bind:active="'routes'" style="z-index: 1000" />
+    <div class="page">
+      <h1>Routes</h1>
+      <div class="container">
+        <h1>Actieve ritten</h1>
+        <div class="container_items">
+          <div class="container_elements">
+            <div class="container_element_picture">
+              <img src="../assets/icons/profile.svg" alt="people" />
+            </div>
+            <div class="container_element_content">
+              <h2>Bram Colleman</h2>
+              <div class="flex_destination">
+                <img src="../assets/icons/route.svg" alt="" />
+                <span>In de Tuinwijk aan het rijden</span>
+              </div>
+            </div>
+            <div class="icons">
+              <img
+                class="target"
+                src="../assets/icons/target.svg"
+                alt="target"
+              />
+              <img
+                class="target"
+                src="../assets/icons/chatblue.svg"
+                alt="chat"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container margin">
+        <h1>Aankomende ritten</h1>
+        <div class="container_items">
+          <div class="container_elements">
+            <div class="container_element_picture">
+              <img src="../assets/icons/profile.svg" alt="people" />
+            </div>
+            <div class="container_element_content">
+              <h2>Bram Colleman</h2>
+              <div class="flex_destination">
+                <img src="../assets/icons/route.svg" alt="" />
+                <span>In de Tuinwijk aan het rijden</span>
+              </div>
+            </div>
+            <div class="icons">
+              <img
+                class="target"
+                src="../assets/icons/target.svg"
+                alt="target"
+              />
+              <img
+                class="target"
+                src="../assets/icons/chatblue.svg"
+                alt="chat"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.margin {
+  margin-left: 1.5rem;
+}
+.icons {
+  padding-left: 1rem;
+  margin-left: auto;
+  padding-right: 0.3rem;
+}
+
+.target {
+  width: 2rem;
+  height: 2rem;
+  padding: 2rem;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+h1 {
+  text-align: left;
+}
+
+span {
+  font-size: 1.5rem;
+}
+
+.flex_destination {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.flex_destination img {
+  width: 2rem;
+  height: 2rem;
+  padding-right: 1rem;
+}
+.container_element_content {
+  line-height: 0.2rem;
+  margin-bottom: 0.7rem;
+}
+
+.container_element_picture img {
+  padding-right: 2rem;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50px;
+}
+.page {
+  background-color: #fafafa;
+}
+
+.container_elements {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 1rem 0;
+  padding: 1rem;
+}
+.container {
+  display: inline-block;
+  padding: 2rem;
+  background-color: #ffffff;
+  border-radius: 15px;
+  margin-top: 2rem;
+}
+
+.container h1 {
+  padding-left: 1rem;
+  text-align: left;
+  margin-top: 1rem;
+}
+
+.container_items {
+  background: #ffffff;
+  box-shadow: 0px 3px 10px 4px rgba(0, 0, 0, 0.06);
+  border-radius: 5px;
+}
+</style>
