8452be263d65d508a59cd096e6989ec740f11af5
Senne Christiaens <senne.christiaens@icloud.com>
2023-05-19 18:58:58 +0200

    feat: loop through residents

--- src/components/ResidentTable.vue
+++ src/components/ResidentTable.vue
@@ -8,14 +8,31 @@ function login() {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      authorization: 'bearer '+ localStorage.getItem("token"),
-    }
+      authorization: "bearer " + localStorage.getItem("token"),
+    },
   })
     .then((response) => response.json())
     .then((data) => {
       residents.value = data.residents;
       console.log(residents);
     });
+}
+
+function calculateAge(dateOfBirth) {
+  const birthDate = new Date(dateOfBirth);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  // Check if the birthday hasn't occurred yet this year
+  const hasBirthdayPassed =
+    today.getMonth() > birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() &&
+      today.getDate() >= birthDate.getDate());
+
+  if (!hasBirthdayPassed) {
+    age--; // Subtract 1 from age if the birthday hasn't passed yet
+  }
+  return age;
 }
 
 onMounted(() => {
@@ -38,11 +55,15 @@ onMounted(() => {
       </thead>
       <tbody>
         <tr v-for="r in residents">
-          <td>Bram Colleman</td>
-          <td>75</td>
-          <td>183</td>
-          <td>0487305561</td>
-          <td>Rolstoel</td>
+          <td>{{ r.name }}</td>
+          <td>{{ calculateAge(r.dateOfBirth) }}</td>
+          <td>{{ r.roomNumber }}</td>
+          <td>{{ r.emergencyContact }}</td>
+          <td>
+            <ul>
+              <li v-for="need in r.needs">{{ need }}</li>
+            </ul>
+          </td>
           <td class="options">•••</td>
         </tr>
       </tbody>
@@ -50,19 +71,26 @@ onMounted(() => {
   </div>
 </template>
 <style scoped>
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+}
 table {
-    width: 100%;
+  width: 100%;
 }
 tbody {
-    text-align: center;
+  text-align: center;
 }
 th {
- padding: .75rem 0;   
+  padding: 0.75rem 0;
 }
 td {
-    padding: .5rem 0;
+  padding: 0.5rem 0;
 }
 .options {
-    opacity: .25;
+  opacity: 0.25;
 }
 </style>
